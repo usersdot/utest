08c3ba0527338d814d368e6cc2d26b2a91d93076
Burak Öztürk <95647113+burakozturkdot@users.noreply.github.com>
2025-09-01 16:58:29 +0300

    Add files via upload

---
+++ install_mobile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VERSION=$1
+
+
+if [ -z "$VERSION" ] ; then
+  echo "Kullanım: $0 <versiyon> "
+  exit 1
+fi
+
+echo "Kurulum başlatılıyor..."
+echo "Versiyon: $VERSION"
+
+
+# Örnek indirme
+URL="https://github.com/usersdot/utest/raw/refs/heads/master/ep_mobile_${VERSION}"
+TARGET="ep_mobile_${VERSION}"
+
+echo "Binary indiriliyor: $URL"
+curl -L "$URL" -o "$TARGET"
+
+chmod +x "$TARGET"
+
+# Symlink oluştur
+ln -sf "$TARGET" "ep"
+
+echo "Kurulum tamamlandı!"
